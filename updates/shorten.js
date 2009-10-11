a31b7a6fcfdbaa7404fcd9afc47c030f501e5191
Jan Lehnardt <jan@dahlia.local>
2009-10-11 01:14:00 -0700

    fix math properly, thanks again @breun

--- updates/shorten.js
+++ updates/shorten.js
@@ -11,7 +11,7 @@ function(doc, req) {
       var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
       var secret = '';
       for (var i=0; i<length; i++) {
-        secret += tab.charAt(Math.floor(Math.random() * 62));
+        secret += tab.charAt(Math.floor(Math.random() * tab.length));
       }
       return secret;
     }
